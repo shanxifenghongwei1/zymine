49c4f4ebbaa3e6479d1e42f69e9d16fb76d64563
shanxifenghongwei1 <867795803@qq.com>
2022-08-22 17:31:52 +0800

    单体电池页面完善

--- src/views/pcsany/energy/singleCell.vue
+++ src/views/pcsany/energy/singleCell.vue
@@ -33,18 +33,25 @@
       </div>
     </div>
 
-    <div class="flex">
+    <div class="flex" v-show="pmlook">
       <!-- 左边的电池簇热力图 -->
       <div class="width-70 bg-bgcardcolor mt30">
-        <div class="bglu color-white p15 flex flex-between">
+        <div class="bglu color-white p15 flex flex-between align-items-center">
           <div>电池簇热力图</div>
-          <div class="flex">
-            <div>显示电压温度图表</div>
-            <div>导出</div>
+          <div class="flex align-items-center">
+            <div
+              class="bgheands pt5 pb5 pl10 pr10 mr10 changheand"
+              @click="lookdy"
+            >
+              显示电压温度图表
+            </div>
+            <div class="flex align-items-center bgheands pt5 pb5 pl10 pr10">
+              <svg-icon class="fs18 mr5" icon-class="xiazhai" /> 导出
+            </div>
           </div>
         </div>
         <!-- 下面的大方框 -->
-        <div class="pt20 pb20 pl10 pr10 hi650 align-content-start">
+        <div class="pt20 pb20 pl10 pr10 hi650 other align-content-start">
           <!-- 一个纵向电池组 -->
           <div
             class="dcfather flex"
@@ -68,6 +75,8 @@
                 "
                 v-for="quer in item.children"
                 :key="quer.xh"
+                :class="acid == quer.id ? 'active' : ''"
+                @click="changeacid(quer)"
               >
                 <div class="smsmtext absolute left top text-center">
                   {{ quer.dy }}
@@ -106,22 +115,85 @@
             color-white
             pl15
             pr15
-            pt5
-            pb5
+            pt10
+            pb10
             flex flex-between
             align-items-center
           "
         >
           <div>电池簇热力图</div>
 
-          <el-date-picker v-model="value1" type="date" placeholder="选择日期">
+          <el-date-picker
+            class="bgheands"
+            v-model="value1"
+            type="date"
+            placeholder="选择日期"
+          >
           </el-date-picker>
+        </div>
+        <!-- 参数 -->
+        <div
+          class="
+            pt20
+            pb20
+            pl10
+            pr10
+            hi650
+            bg-bgcardcolor
+            align-content-start
+            color-white1
+          "
+        >
+          <div class="flex width-full text-center pt10 pb10 bghui">
+            <div class="flex-1">参数</div>
+            <div class="flex-1">最大值</div>
+            <div class="flex-1">最小值</div>
+          </div>
+          <!-- 第二行的 -->
+          <div class="flex width-full text-center pt10 pb10 bghei">
+            <div class="flex-1">电压</div>
+            <div class="flex-1">3.349</div>
+            <div class="flex-1">3.231</div>
+          </div>
+          <!-- 第三行的 -->
+          <div class="flex width-full text-center pt10 pb10 bghui">
+            <div class="flex-1">温度</div>
+            <div class="flex-1">-40</div>
+            <div class="flex-1">-40</div>
+          </div>
+
+          <!-- 电压图表 -->
+          <div class="pt10">
+            <div class="pt10">电压（V）</div>
+            <div id="dytb"></div>
+          </div>
+
+          <!-- 温度图表 -->
+          <div class="pb10">
+            <div class="pt10">温度（℃）</div>
+            <div id="wdtb"></div>
+          </div>
         </div>
       </div>
     </div>
 
-    <div class="color-white">
+    <div class="color-white mt20 width-full" v-if="!pmlook">
+      <div class="bglu color-white p15 flex flex-between align-items-center">
+        <div>当前电压温度图表</div>
+        <div class="flex align-items-center">
+          <div
+            class="bgheands pt5 pb5 pl10 pr10 mr10 changheand"
+            @click="lookreli"
+          >
+            显示热力图
+          </div>
+          <div class="flex align-items-center bgheands pt5 pb5 pl10 pr10">
+            <svg-icon class="fs18 mr5" icon-class="xiazhai" /> 导出
+          </div>
+        </div>
+      </div>
       <div id="chartsz"></div>
+      <div id="chartsx"></div>
     </div>
   </div>
 </template>
@@ -142,20 +214,50 @@ export default {
       ],
       value: "",
       value1: "",
+      acid: "0151",
+      pmlook: true,
     };
   },
   mounted() {
-    this.initcharts();
-
     console.log(this.liefor(), "当前素组");
+    setTimeout(() => {
+      this.initdytub();
+      this.initwdtub();
+    }, 300);
   },
   methods: {
+    lookdy() {
+      this.pmlook = false;
+      setTimeout(() => {
+        this.initcharts();
+        this.initcharts1();
+      }, 300);
+    },
+    lookreli() {
+      this.pmlook = true;
+
+      setTimeout(() => {
+        this.initdytub();
+        this.initwdtub();
+      }, 300);
+    },
+    changeacid(quer) {
+      this.acid = quer.id;
+    },
+    // 电压图表放大
     initcharts() {
       var myChart = this.$echarts.init(
         document.getElementById("chartsz"),
         "dark"
       );
       let option = {
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow",
+          },
+        },
+        color: "#527f3e",
         xAxis: {
           type: "category",
           boundaryGap: false,
@@ -163,6 +265,197 @@ export default {
         },
         yAxis: {
           type: "value",
+          name: "电压（V）",
+        },
+        series: [
+          {
+            data: this.xlist().y,
+            type: "line",
+            areaStyle: {},
+          },
+        ],
+      };
+
+      myChart.setOption(option);
+    },
+    // 温度图表放大
+    initcharts1() {
+      var myChart = this.$echarts.init(
+        document.getElementById("chartsx"),
+        "dark"
+      );
+      let option = {
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow",
+          },
+        },
+        color: "#527f3e",
+        xAxis: {
+          type: "category",
+          boundaryGap: false,
+          data: this.xlist().x,
+        },
+        yAxis: {
+          type: "value",
+          name: "温度（℃）",
+        },
+        series: [
+          {
+            data: this.xlist().y,
+            type: "line",
+            areaStyle: {},
+          },
+        ],
+      };
+
+      myChart.setOption(option);
+    },
+    // 电压图表
+    initdytub() {
+      var myChart = this.$echarts.init(document.getElementById("dytb"));
+      let option = {
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow",
+          },
+        },
+        color: "#527f3e",
+        xAxis: {
+          type: "category",
+          boundaryGap: false,
+          data: this.xlist().x,
+
+          show: true,
+          nameTextStyle: {
+            color: "#fff",
+            fontSize: 15,
+            backgroundColor: "#fff",
+          },
+
+          // 是否展示坐标轴
+          axisLine: {
+            show: true,
+            lineStyle: {
+              color: "#fff",
+              width: 1,
+              type: "solid",
+            },
+          },
+          axisTick: {
+            show: true,
+            alignWithLabel: true,
+          },
+          axisLabel: {
+            color: "#fff",
+          },
+        },
+        yAxis: {
+          type: "value",
+
+          show: true,
+          nameTextStyle: {
+            color: "#fff",
+            fontSize: 15,
+            backgroundColor: "#fff",
+          },
+
+          // 是否展示坐标轴
+          axisLine: {
+            show: true,
+            lineStyle: {
+              color: "#fff",
+              width: 1,
+              type: "solid",
+            },
+          },
+          axisTick: {
+            show: true,
+            alignWithLabel: true,
+          },
+          axisLabel: {
+            color: "#fff",
+          },
+        },
+        series: [
+          {
+            data: this.xlist().y,
+            type: "line",
+            areaStyle: {},
+          },
+        ],
+      };
+
+      myChart.setOption(option);
+    },
+    // 电压温度图表
+    initwdtub() {
+      var myChart = this.$echarts.init(document.getElementById("wdtb"));
+      let option = {
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow",
+          },
+        },
+        color: "#527f3e",
+        xAxis: {
+          type: "category",
+          boundaryGap: false,
+          data: this.xlist().x,
+
+          show: true,
+          nameTextStyle: {
+            color: "#fff",
+            fontSize: 15,
+            backgroundColor: "#fff",
+          },
+
+          // 是否展示坐标轴
+          axisLine: {
+            show: true,
+            lineStyle: {
+              color: "#fff",
+              width: 1,
+              type: "solid",
+            },
+          },
+          axisTick: {
+            show: true,
+            alignWithLabel: true,
+          },
+          axisLabel: {
+            color: "#fff",
+          },
+        },
+        yAxis: {
+          type: "value",
+
+          show: true,
+          nameTextStyle: {
+            color: "#fff",
+            fontSize: 15,
+            backgroundColor: "#fff",
+          },
+
+          // 是否展示坐标轴
+          axisLine: {
+            show: true,
+            lineStyle: {
+              color: "#fff",
+              width: 1,
+              type: "solid",
+            },
+          },
+          axisTick: {
+            show: true,
+            alignWithLabel: true,
+          },
+          axisLabel: {
+            color: "#fff",
+          },
         },
         series: [
           {
@@ -188,53 +481,25 @@ export default {
     },
     // 总数据
     mokeList() {
-      let a = {
-        values: "40.000℃",
-        numid: 1,
-        children: [
-          { dy: "3.161", xh: 1 },
-          { dy: "3.161", xh: 2 },
-          { dy: "3.161", xh: 3 },
-          { dy: "3.161", xh: 4 },
-          { dy: "3.161", xh: 5 },
-          { dy: "3.161", xh: 6 },
-          { dy: "3.161", xh: 7 },
-          { dy: "3.161", xh: 8 },
-          { dy: "3.161", xh: 9 },
-          { dy: "3.161", xh: 10 },
-          { dy: "3.161", xh: 11 },
-          { dy: "3.161", xh: 12 },
-          { dy: "3.161", xh: 13 },
-        ],
-      };
-
       let b = [];
-      let children = [];
+
       for (let i = 0; i < 33; i++) {
-        for (let m = 0; m < 20; m++)
-          // b.push(a);
-          b.push({
-            values: "40.000℃",
-            numid: 1,
-            children: [
-              { dy: "3.161", xh: 1 },
-              { dy: "3.161", xh: 2 },
-              { dy: "3.161", xh: 3 },
-              { dy: "3.161", xh: 4 },
-              { dy: "3.161", xh: 5 },
-              { dy: "3.161", xh: 6 },
-              { dy: "3.161", xh: 7 },
-              { dy: "3.161", xh: 8 },
-              { dy: "3.161", xh: 9 },
-              { dy: "3.161", xh: 10 },
-              { dy: "3.161", xh: 11 },
-              { dy: "3.161", xh: 12 },
-              { dy: "3.161", xh: 13 },
-            ],
-          });
+        b.push({
+          values: "40.000℃",
+          numid: 1,
+          children: this.chiles(i),
+        });
       }
 
       return b;
+    },
+    // 13 节电池
+    chiles(i) {
+      let children = [];
+      for (let m = 0; m < 13; m++) {
+        children.push({ dy: "3.161", xh: m + 1, id: m + "15" + i });
+      }
+      return children;
     },
     //  分布成列
     liefor() {
@@ -256,8 +521,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.bgheands {
+  background: rgb(88, 139, 36);
+}
 .dcfather {
   width: 650px;
+  .active {
+    background: rgb(20, 83, 74);
+  }
+}
+.bghui {
+  background: rgb(70, 70, 82);
+}
+.bghei {
+  background: rgb(47, 47, 55);
 }
 .sssss {
   width: 20px;
@@ -289,7 +566,18 @@ export default {
   width: 100%;
   height: 350px;
 }
-
+#chartsx {
+  width: 100%;
+  height: 350px;
+}
+#dytb {
+  width: 100%;
+  height: 230px;
+}
+#wdtb {
+  width: 100%;
+  height: 230px;
+}
 .emsx {
   padding: 50px;
   height: 100%;
@@ -303,8 +591,12 @@ export default {
 }
 .hi650 {
   height: 650px;
+
   // overflow: hidden;
   // overflow-y: scroll;
+}
+
+.other {
   overflow: auto;
   &::-webkit-scrollbar {
     display: block !important;
@@ -314,7 +606,7 @@ export default {
 
   /* 设置滚动条的轨道 */
   &::-webkit-scrollbar-track {
-    background: #04614f;
+    background: #606866;
     border-radius: 2px;
   }
   /* 设置滚动条里面的小方块 */
